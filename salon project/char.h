fd988dd0680ce59828174a6d3a15a8ff0124e719
rubensyema <rubensyema2019@gmail.com>
2026-02-16 11:20:39 +0700

    Create char.h

---
+++ salon project/char.h
@@ -0,0 +1,34 @@
+#define BR_VERTICAL_LIGHT "│"
+#define BR_VERTICAL_HEAVY "┃"
+#define BR_VERTICAL_BOLD "▉"
+#define BR_VERTICAL_FULLBLOCK "█"
+#define BR_VERTICAL_LOW_SEV "▉"
+
+#define BR_HORIZONTAL_LIGHT "─"
+#define BR_HORIZONTAL_HEAVY "━"
+
+/**
+ *  @brief Box Drwaing Light Down and Right
+ */
+#define BR_LD_R "┌"
+
+/**
+ *  @brief Box Drwaing heavy Down and Right
+ */
+#define BR_HD_R "┏"
+
+
+/**
+ *  @brief Box Drwaing heavy Down and Left
+ */
+#define BR_LD_L "┐"
+
+/**
+ *  @brief Box Drwaing heavy Down and Left
+ */
+#define BR_HD_L "┓"
+
+#define BR_HD_BR "┗"
+#define BR_HD_BL "┛"
+
+#define TR_UP "▲"
